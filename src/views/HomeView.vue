b9a1501eb78ab0edfe7d9649934d964e836e4691
Azim Pathan <azimasifpathan@gmail.com>
2023-05-24 18:11:05 +0530

    ecommerce

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <NavebarView class="index" />
+    <DashBoard />
+    <FooterView />
+  </v-app>
+</template>
+
+<script>
+  import NavebarView from '../components/NavebarView'
+  import DashBoard from '../components/DashBoard'
+  import FooterView from '../components/FooterView'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      NavebarView,
+      DashBoard,
+      FooterView
+
+    },
+  }
+</script>
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
